de98c8f4a1ffeb41b26ff8a0da0c7da983378735
ame <shachi10969@gmail.com>
2022-11-13 22:01:17 +0900

    Initial Commit

---
+++ ReplacementText/ReplacementTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReplacementTextApp.swift
+//  ReplacementText
+//
+//  Created by t&a on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct ReplacementTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
